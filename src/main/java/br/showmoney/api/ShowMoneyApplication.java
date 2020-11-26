f0649f5cbbf98275573973acc82a314cc1fee28b
PatrickGmailHub <pgo.guedes@gmail.com>
2020-11-26 02:44:45 -0300

    Liberando o CORS na aplicação

--- src/main/java/br/showmoney/api/ShowMoneyApplication.java
+++ src/main/java/br/showmoney/api/ShowMoneyApplication.java
@@ -2,6 +2,9 @@ package br.showmoney.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ShowMoneyApplication {
@@ -9,5 +12,20 @@ public class ShowMoneyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShowMoneyApplication.class, args);
 	}
+
+	@Bean
+  public WebMvcConfigurer corsConfigurer() {
+      return new WebMvcConfigurer() {
+          @Override
+          public void addCorsMappings(CorsRegistry registry) {
+              registry.addMapping("/**");//.allowedOrigins("http://localhost:8000");
+              
+							/*
+							 * registry.addMapping("/**").allowedMethods("GET", "POST", "PUT",
+							 * "DELETE").allowedOrigins("*") .allowedHeaders("*");
+							 */
+          }
+      };
+  }
 
 }
